d87ec42b1cbb0806fe3d63b7d82637fb7abc1d90
Robson Farias <robsonfariasAD@gmail.com>
2022-01-24 23:52:12 -0300

    checkpoint

--- ceps.php
+++ ceps.php
@@ -8,4 +8,4 @@ $obApi = new Api("https://cep.awesomeapi.com.br");
 // retorna dados do cep
 $result = $obApi->send("GET","/json/05424020");
 
-echo '<pre>'; print_r($result); echo '</pre>'; exit;
+print_r($result);
--- currency-quote.php
+++ currency-quote.php
@@ -8,4 +8,4 @@ $obApi = new Api("https://economia.awesomeapi.com.br/json");
 // Retorna a ultima ocorrência das moedas selecionadas
 $result = $obApi->send("GET","/last/USD-BRL,EUR-BRL,BTC-BRL");
 
-echo '<pre>'; print_r($result); echo '</pre>'; exit;
+print_r($result);
